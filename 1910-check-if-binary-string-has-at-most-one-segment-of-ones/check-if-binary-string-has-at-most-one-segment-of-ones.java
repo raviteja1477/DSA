18b0a71e0db93855bcf8ec71130c076980a33da1
Chintham Reddy Venkata Ravi Teja reddy <tejareddyravi07@gmail.com>
2026-09-10 18:04:11 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (35.43%) - LeetSync

--- 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.java
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.java
@@ -1,13 +1,10 @@
 class Solution {
     public boolean checkOnesSegment(String s) {
-        int count = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '1' && (i == 0 || s.charAt(i - 1) == '0')) {
-                count++;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == '1' && s.charAt(i - 1) == '0') {
+                return false;
             }
         }
-
-        return count <= 1;
+        return true;
     }
 }
